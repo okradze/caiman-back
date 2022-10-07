287eb93ba029c9ce13221c693aaf6be91de8a4ad
levani-k <levaniqistauri@gmail.com>
2022-10-07 21:15:59 +0400

    chore: get port from env var if it is there or use 8000 by defualt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app')
+const PORT = process.env.PORT || 8000
 
-app.listen(8000, () => {
-  console.log('Server is running')
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
