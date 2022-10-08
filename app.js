ac0539bfe700a745abc1d685ac6a54f2ca1e4f01
levani-k <levaniqistauri@gmail.com>
2022-10-08 09:37:50 +0400

    feat: add email and numebr service. create request_loan endpoint for locan creation

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config({path:__dirname+'/.env'})
+const { createUser, notifyUser } = require('./utils')
+
+// NOTE: we need those from in process.env
+
 const app = express()
+
+app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 
 app.get('/', (req, res) => {
   res.send('OK!')
+})
+
+app.post('/request_loan', async (req, res) => {
+  const { body } = req
+  const newUser = createUser(body)
+  const { phone, email, _id } = newUser
+  const {
+		emailNotifySuccess,
+		numberNotifySuccess,
+	} = await notifyUser({
+    phone,
+    email,
+    redirectTo: `${process.env.FRONT_URL}/?id=${_id}`
+  })
+
+  if (emailNotifySuccess && numberNotifySuccess) {
+    res.send('success')
+  } else if (emailNotifySuccess) {
+    res.send('email succedded')
+  } else if (numberNotifySuccess) {
+    res.send('number succedded')
+  }  else {
+    res.send('failed').status(500)
+  }
+
 })
 
 module.exports = app
---
+++ utils.js
@@ -0,0 +1,90 @@
+require('dotenv').config({path:__dirname+'/.env'})
+const sgMail = require('@sendgrid/mail')
+const { v4: uuidv4 } = require('uuid')
+const fs = require('fs')
+const util = require('util')
+const handlebars = require('handlebars')
+const mjml = require('mjml')
+
+const twilioClient = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKKEN)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const readFile = util.promisify(fs.readFile)
+
+const users = []
+
+const createUser = (body) => {
+    const newUser = {
+        _id: uuidv4(),
+        firstName: body.firstName,
+        lastName: body.lastName,
+        personalNumber: body.personalNumber,
+        dateOfBirth: body.dateOfBirth,
+        phone: body.phone,
+        email: body.email,
+    }
+    users.push(newUser)
+
+    return newUser
+}
+
+  /**
+   * this method is reusable for wrapping msg object
+   * and sending it using sendgrid
+   * @param sendMailObject
+   */
+const sendEmail = async ({ email, redirectTo }) => {
+	try {
+		const mjmlTemplateFile = await readFile(`./verify.hbs`, 'utf8')
+		const template = handlebars.compile(mjmlTemplateFile)
+
+		const hbsHtml = template({ redirectTo })
+		const templateMarkup = mjml(hbsHtml)
+		const emailRes = await sgMail.send({
+			to: email,
+			from: 'levaniqistauri29@gmail.com',
+			subject: 'Verify user',
+			text: 'please verify your self so we can transfer money safely',
+			html: templateMarkup.html,
+		})
+		return true
+	} catch (error) {
+		return false
+	}
+}
+
+  /**
+   * this method sends message and returns boolean
+   * wether it was sent or not
+   * @param sendSmsParams
+   */
+const sendMessage = async ({ phone, redirectTo }) => {
+	try {
+		const messageTemplate = `open this link and verify your profile to get moneny \n ${redirectTo}`
+		const res = await twilioClient.messages.create({
+			body: messageTemplate, 
+			from: '+16403338654',
+			to: `+995${phone}`,
+		})
+		return !!res
+	} catch (err) {
+		console.log(err)
+		return false
+	}
+}
+
+const notifyUser = async ({ phone, email, redirectTo }) => {
+	// const numberNotifySuccess = await sendMessage({ phone, redirectTo })
+	// const emailNotifySuccess = await sendEmail({ email, redirectTo })
+	const numberNotifySuccess = true
+	const emailNotifySuccess = true
+
+	return {
+		emailNotifySuccess,
+		numberNotifySuccess,
+	}
+}
+
+module.exports = {
+    createUser,
+    notifyUser,
+}
